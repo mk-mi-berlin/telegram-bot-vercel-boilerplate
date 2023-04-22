ffe7e6aaa1427cab8800e991af895289aef778c7
matkl1 <mk@medieninformatik.berlin>
2023-04-22 21:03:26 +0200

    storephoto2d

--- src/photo/storephoto.ts
+++ src/photo/storephoto.ts
@@ -34,7 +34,7 @@ const storephoto = (ctx) => async (ctx2) => {
     var picture = ctx2.message.photo[ctx2.message.photo.length - 1].file_id;
     var url = "https://api.telegram.org/bot" + BOT_TOKEN + "/getFile?file_id=" + picture;
     console.log("storephoto333");
-    let x =  await ctx.telegram.getFileLink(picture).then(async url =>  {
+    let x =  await ctx2.telegram.getFileLink(picture).then(async url =>  {
         console.log("storephoto mkGetfileLink: " + url);
         axios({ url, responseType: 'stream' })
           .then(response => {
